96bac077046a31c0821dd03e0ec4c7a889e51aba
patrick-ta <patrick.ta451@gmail.com>
2024-09-24 14:57:53 -0400

    Added logout functionality

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,39 @@
 import './Navbar.css'
+import { useState, useEffect } from 'react';
+import { checkAuth } from '../../services/authService'
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+    const [isAuthenticated, setIsAuthenticated] = useState(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+    const verifyRole = async () => {
+        const data = await checkAuth();
+        console.log(data.isAuthenticated, data.isModerator)
+        setIsAuthenticated(data.isAuthenticated);
+    }
+
+    verifyRole();
+    }, []);
+
+    const handleLogout = async () => {
+        try {
+            const response = await fetch('http://localhost:5000/api/auth/logout', {
+                method: 'POST',
+                credentials: 'include',
+                headers: {
+                    'Content-Type': 'application/json', 
+                },
+                body: JSON.stringify({}), 
+            });
+            navigate('/');
+        } catch(error) {
+            console.log(error);
+        }
+        
+    }
+
     return (
         <>
         <nav className='flex'>
@@ -9,7 +42,9 @@ const Navbar = () => {
                 <li>Quests</li>
                 <li>Submit</li>
             </ul>
-            <h3 className='login'>Login</h3>
+            
+            {isAuthenticated ? <button onClick={handleLogout} className='login'>Logout</button> : <button onClick={() => navigate('/login')} className='login'>Login</button>}
+            
         </nav>
         </>
     )
